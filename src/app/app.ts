2c6c7d7c5ed67500dc4cdef996004259d8c98a96
Daniel Stollnberger <daniiistolliii@gmail.com>
2026-02-21 23:43:13 +0100

    sidenav & toolbar implemented and designed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports:
+    [
+      RouterOutlet,
+      MatToolbarModule,
+      MatSidenavModule,
+      MatIconModule,
+    ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('simple-crm');
+  showFiller = false;
 }
