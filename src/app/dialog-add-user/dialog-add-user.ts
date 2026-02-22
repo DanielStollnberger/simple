36fa5c79dd17eee95eb855ba7afee2789196b7b4
Daniel Stollnberger <daniiistolliii@gmail.com>
2026-02-22 01:24:43 +0100

    user dialog added

---
+++ src/app/dialog-add-user/dialog-add-user.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { MatDialogContent, MatDialogActions } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+@Component({
+  selector: 'app-dialog-add-user',
+  imports: [
+    MatDialogContent,
+    MatDialogActions,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  templateUrl: './dialog-add-user.html',
+  styleUrl: './dialog-add-user.scss',
+})
+export class DialogAddUser {
+
+}
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { DialogAddUser } from '../dialog-add-user/dialog-add-user';
+import {
+  MatDialog,
+} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user',
   imports: [
     MatIconModule,
     MatButtonModule,
-    MatTooltipModule
+    MatTooltipModule,
   ],
   templateUrl: './user.html',
   styleUrl: './user.scss',
 })
-export class User {
 
+export class User {
+  readonly dialog = inject(MatDialog);
+  openDialog() {
+    this.dialog.open(DialogAddUser)
+  }
 }
